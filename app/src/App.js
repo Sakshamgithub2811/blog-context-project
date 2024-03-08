6a91ed2d725b68f0ecc0c720b74d5c63a5da302f
Saksham Shrivastava <146808515+sakshamshrivastava28111810@users.noreply.github.com>
2024-03-08 13:15:03 +0530

    08-03-2024

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,20 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import Blogs from './components/Blogs';
+import Pagination from './components/Pagination';
+
 import './App.css';
+import { AppContext } from './context/AppContext';
+import { useContext, useEffect } from 'react';
+
 
 function App() {
+
+  const {fetchBlogPosts} = useContext(AppContext);
+
+  useEffect(()=>{
+fetchBlogPosts();
+  },[])
+
   return (
     <div>
       <Header/>
--- app/src/components/Blogs.jsx
+++ app/src/components/Blogs.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import Spinner from './Spinner';
+
 
 const Blogs = () => {
+  const { loading, posts } = useContext(AppContext); // Assuming posts are retrieved from the context
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      {loading ? (
+        <Spinner />
+      ) : (
+        posts.length === 0 ? (
+          <div>
+            <p>No Post Found</p>
+          </div>
+        ) : (
+          posts.map((post) =>(
+            <div key={post.id}>
+              <p>{post.title}</p>
+              <p>
+                By<span>{post.author}</span> on<span>{post.category}</span>
+              </p>
+              <p>Posted on{post.date}</p>
+              <p>{post.content}</p>
+              <div>
+                {
+                  post.tags.map((tag,index)=>{
+                    return <span key={index}>(`#${tag}`)</span>
+                  })
+                }
+              </div>
+            </div>
 
-export default Blogs
+          )) // Assuming each post has a unique identifier (id)
+        )
+      )}
+    </div>
+  );
+};
+
+export default Blogs;
