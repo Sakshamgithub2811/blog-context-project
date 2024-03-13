f9b0986e6c921859c2d875fac1c3d95a87904a54
Saksham Shrivastava <146808515+sakshamshrivastava28111810@users.noreply.github.com>
2024-03-13 13:17:18 +0530

    dark mode

--- app/src/components/Pagination.js
+++ app/src/components/Pagination.js
@@ -3,19 +3,19 @@ import { AppContext } from '../context/AppContext';
 
 const Pagination = () => {
 
-  const {page,handlePageChange,totalPages} = useContext(AppContext);
+  const {page,handlePageChange,totalPages,darkMode,toggleDarkMode} = useContext(AppContext);
   return ( 
     <div>
-      <div>
+      <div >
         {
           page>1 &&
-          <button n className="rounded-md border p-x-4 p-y-2" onClick={()=>handlePageChange(page-1)} >
+          <button  className="rounded-md border p-x-4 p-y-2" onClick={()=>handlePageChange(page-1)} >
             Previous
           </button>
         }
-         {
+        {
           page< totalPages &&
-          <button onClick={()=>handlePageChange(page+1)}>
+          <button className="rounded-md border p-x-4 p-y-2" onClick={()=>handlePageChange(page+1)}>
             Next
           </button>
         }
@@ -23,6 +23,15 @@ const Pagination = () => {
         <p>
           Page{page} of {totalPages}
         </p>
+
+        <button onClick={toggleDarkMode} className={`bg-gray-200 dark:bg-gray-800 ${darkMode ? 'text-white' : 'text-black'}`}>
+          {darkMode ?"DRK":"LIT"  }
+          ${darkMode}
+   
+        </button>
+       
+
+
 
       </div>
       
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   content: [   './src/**/*.{html,js}',
 ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          100: '#f5f5f5',
+          200: '#d4d4d4',
+          300: '#b3b3b3',
+          400: '#737373',
+          500: '#525252', 
+          600: '#404040',
+          700: '#2b2b2b',
+          800: '#1f1f1f',
+          900: '#141414',
+        },
+      },
+    },
   },
+  darkMode: 'class', 
   plugins: [],
 }
